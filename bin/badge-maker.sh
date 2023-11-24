4e1b55dcec7e2ebfabccdf0c22be284249f89d66
Yasu Watanabe <ywata@protonmail.com>
2023-11-24 16:28:59 +0900

    debug

--- bin/badge-maker.sh
+++ bin/badge-maker.sh
@@ -4,6 +4,5 @@ if [ $# -gt 2 ]; then
     repo=$1
     badge_format=$2
     shift 2
-    git branch -r
     git branch -r --sort=committerdate | bin/badge-markdown.py markdown --repository ${repo} --badge-format ${badge_format} --workflows $@
 fi    
